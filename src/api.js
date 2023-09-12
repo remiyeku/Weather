5281d5829593ff2d79d4cd9d4036eb8bf0b166b4
remiyeku <antakin4@gmail.com>
2023-09-12 12:58:25 +0100

    search component working in console but AsyncPaginate options error stops page from rendering

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "8bd39897a6mshc76471eecb749f8p19f8a1jsnb5f19d2b8b11",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,5 +1,31 @@
-const Search = () => {
-  return <div></div>;
-};
+import { AsyncPaginate } from "react-select-async-paginate";
+import { useState } from "react";
+import { GEO_API_URL, geoApiOptions } from "../../api";
+
+function Search({ onSearchChange }) {
+  const [search, setSearch] = useState(null);
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+  };
+  const loadOptions = (inputValue) => {
+    fetch(
+      `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search For Cities"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+}
 
 export default Search;
