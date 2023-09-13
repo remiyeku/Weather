910df6cd5b5fcbfc93c5347762f5bf56e95cafe0
remiyeku <109895011+remiyeku@users.noreply.github.com>
2023-09-13 10:49:30 +0100

    Delete src/api.js

--- src/api.js
+++
@@ -1,9 +0,0 @@
-export const geoApiOptions = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "8bd39897a6mshc76471eecb749f8p19f8a1jsnb5f19d2b8b11",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  },
-};
-
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
