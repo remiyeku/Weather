679bcb148667f179f7b63ee2b103b6751c49e33d
remiyeku <antakin4@gmail.com>
2023-09-27 16:59:55 +0100

    functionality for forecast complete

---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,59 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+import "./forecast.css";
+
+const weekDays = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+function Forecast({ data }) {
+  const daysOfWeek = new Date().getDay();
+  const nextDays = weekDays
+    .slice(daysOfWeek, weekDays.length)
+    .concat(weekDays.slice(0, daysOfWeek));
+
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, index) => (
+          <AccordionItem key={index}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    alt="forecasted weather"
+                    className="icon-small"
+                    src={`icons/${item.weather[0].icon}.png`}
+                  />
+                  <label className="days">{weekDays[index]}</label>
+                  <label className="description">
+                    {item.weather[0].description}
+                  </label>
+                  <label className="min-max">
+                    {Math.round(item.main.temp_min)}°C /
+                    {Math.round(item.main.temp_max)}°C
+                  </label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+}
+
+export default Forecast;
