fcea18ba0484675df72ace5bdf193820d96996ae
remiyeku <antakin4@gmail.com>
2024-01-31 15:12:26 +0000

    updated env variables

--- src/api.js
+++ src/api.js
@@ -1,19 +1,11 @@
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/";
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const geoApiOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": process.env.GEO_API_KEY,
-    "X-RapidAPI-Host": process.env.GEO_API_HOST,
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
-
-try {
-  const response = await fetch(GEO_API_URL, geoApiOptions);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
-}
 
 export const WEATHER_API_URL = process.env.WEATHER_API_URL;
 export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
