33feb6cb831a898f982659409aaf798acc91093b
remiyeku <antakin4@gmail.com>
2024-01-31 15:08:07 +0000

    updated env variables

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/";
 export const geoApiOptions = {
   method: "GET",
   headers: {
@@ -6,7 +7,13 @@ export const geoApiOptions = {
   },
 };
 
-export const GEO_API_URL = process.env.GEO_API_URL;
+try {
+  const response = await fetch(GEO_API_URL, geoApiOptions);
+  const result = await response.text();
+  console.log(result);
+} catch (error) {
+  console.error(error);
+}
 
 export const WEATHER_API_URL = process.env.WEATHER_API_URL;
 export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
